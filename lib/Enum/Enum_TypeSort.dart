10609cf58ee7cf96e87c37b92aeb455c51457332
Marcko1288 <87115589+Marcko1288@users.noreply.github.com>
2024-05-28 17:07:22 +0200

    Create Enum_TypeSort.dart

---
+++ lib/Enum/Enum_TypeSort.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+///Le tipologie di valore che può assumere:
+///
+/// UP --> Crescente
+///
+/// DO --> Descrescente
+
+
+enum TypeSort {
+  UP,
+  DO,
+}
