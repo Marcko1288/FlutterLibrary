cd544fdd8b052076196ab20ef5ff19149392a4a5
Marcko1288 <87115589+Marcko1288@users.noreply.github.com>
2024-05-28 17:20:00 +0200

    Update Extension_List.dart

--- lib/Extension/Extension_List.dart
+++ lib/Extension/Extension_List.dart
@@ -6,9 +6,9 @@ extension CustomSortExtension<T> on List<T> {
     sort((a, b) {
       int compareResult = keySelector(a).compareTo(keySelector(b));
       switch (typeSort) {
-        case TypeQuery.UP:
+        case TypeSort.UP:
           return compareResult <= 0 ? -1 : 1;
-        case TypeQuery.DO:
+        case TypeSort.DO:
           return compareResult >= 0 ? -1 : 1;
         default:
           return 0;
