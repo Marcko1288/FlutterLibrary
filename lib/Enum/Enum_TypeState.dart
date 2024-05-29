3699014b444e001073459a9798834a9647d9e03a
Marcko1288 <87115589+Marcko1288@users.noreply.github.com>
2024-05-29 16:45:10 +0200

    Create Enum_TypeState.dart

---
+++ lib/Enum/Enum_TypeState.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+///Le tipologie di valore che può assumere:
+///
+/// I --> Insert
+///
+/// M --> Modify
+///
+/// C --> Cancel
+///
+/// R --> Read
+
+enum TypeDate {
+  I,
+  M,
+  C,
+  R,
+}
