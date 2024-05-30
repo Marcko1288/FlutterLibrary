1e3a9e9a987022f4d73fbcf668fc614fafcb3400
Marcko1288 <87115589+Marcko1288@users.noreply.github.com>
2024-05-30 15:19:04 +0200

    Update Extension_Map.dart

--- lib/Extension/Extension_Map.dart
+++ lib/Extension/Extension_Map.dart
@@ -1,28 +1,42 @@
 import 'package:flutter/material.dart';
 
-extension MapExtensions<K, V> on Map<K, V> {
-String convertJsonToString(K key) {
-  return this[key] is String ? this[key] : '';
-}
+import 'package:flutter/material.dart';
 
-DateTime convertJsonToDate(K key) {
-  try {
-    DateTime.parse(this[key]);
-    return DateTime.parse(this[key]);
-  } catch (e) {
+extension MapExtensions<K, V> on Map<K, V> {
+  String convertJsonToString(K key) {
+    return this[key] is String ? this[key] : '';
+  }
+
+  DateTime convertJsonToDate(K key) {
+    if (this[key] is String) {
+      try {
+        return DateTime.parse(this[key]);
+      } catch (e) {
+        // Fall back to current date if parsing fails
+      }
+    }
     return DateTime.now();
   }
-}
 
-double convertJsonToDouble(K key) {
-  return this[key] is double ? this[key] : 0.0;
-}
+  double convertJsonToDouble(K key) {
+    if (this[key] is double) {
+      return this[key];
+    } else if (this[key] is num) {
+      return (this[key] as num).toDouble();
+    }
+    return 0.0;
+  }
 
-int convertJsonToInt(K key) {
-  return this[key] is int ? this[key] : 0;
-}
+  int convertJsonToInt(K key) {
+    if (this[key] is int) {
+      return this[key];
+    } else if (this[key] is num) {
+      return (this[key] as num).toInt();
+    }
+    return 0;
+  }
 
-bool convertJsonToBool(K key) {
-  return this[key] is bool ? this[key] : false;
-}
+  bool convertJsonToBool(K key) {
+    return this[key] is bool ? this[key] : false;
+  }
 }
