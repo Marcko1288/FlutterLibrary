9367d0fbb2542b59418cda53fa04c913fa19b968
Marcko1288 <87115589+Marcko1288@users.noreply.github.com>
2024-06-04 17:34:45 +0200

    Update TextFieldCustom.dart

--- lib/Model/TextFieldCustom.dart
+++ lib/Model/TextFieldCustom.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+/* import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:form_builder_validators/form_builder_validators.dart';
 import 'package:beumed/Library/Extension_String.dart';
@@ -128,3 +128,4 @@ extension ExtTypeValidator on TypeValidator {
   }
 }
 
+ */
