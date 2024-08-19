f5bff411f6a4b1eb26fb5c1ea54c7f2a3da1cc3b
Marcko1288 <87115589+Marcko1288@users.noreply.github.com>
2024-08-19 10:49:09 +0200

    Create Func_JumpYAss.dart

---
+++ lib/Function/Func_JumpYAss.dart
@@ -0,0 +1,34 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutterlibrary/Extension/Extension_Double.dart';
+
+// Funzione per calcolare il valore del salto
+double dynamicJumpYAss<T>(List<T> array, double Function(T) getValue) {
+  // Valori predefiniti
+  double maxV = 100;
+  double minV = 1;
+
+  // Controlla se l'array non è vuoto
+  if (array.isNotEmpty) {
+    maxV = array.map(getValue).reduce((value, element) => value > element ? value : element);
+    minV = array.map(getValue).reduce((value, element) => value < element ? value : element);
+  }
+
+  // Arrotonda i valori minimo e massimo
+  //var roundedYAxisMaxValue = roundUp(maxV, 2);
+  //var roundedYAxisMinValue = roundUp(minV, 2);
+  maxV.roundUp(2);
+  minV.roundUp(2);
+
+  // Calcola il valore del salto (stride)
+  //var strideValue = (roundedYAxisMaxValue - roundedYAxisMinValue) / 5.0;
+  var strideValue = (maxV - minV) / 5.0;
+
+  // Verifica che il valore del salto non sia NaN o zero
+  if (strideValue.isNaN || strideValue == 0) {
+    return 10.0;
+  } else {
+    return strideValue;
+  }
+}
