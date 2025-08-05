8d2df975bdc9f31e1ac82e73b2c1032e347e35e7
Marcko1288 <87115589+Marcko1288@users.noreply.github.com>
2025-08-05 12:15:18 +0200

    Add files via upload

---
+++ lib/link.dart
@@ -0,0 +1,35 @@
+// Link per accedere a tutti i file della libreria
+//export '../Enum/';
+
+// Enum
+export '../Enum/Enum_InputDecorationCustom.dart';
+export '../Enum/Enum_TypeDate.dart';
+export '../Enum/Enum_TypeExtraction.dart';
+export '../Enum/Enum_TypeFormatDate.dart';
+export '../Enum/Enum_TypeQuery.dart';
+export '../Enum/Enum_TypeSort.dart';
+
+
+// Extension
+export '../Extension/Extension_Date.dart';
+export '../Extension/Extension_Double.dart';
+export '../Extension/Extension_List.dart';
+export '../Extension/Extension_Map.dart';
+export '../Extension/Extension_Object.dart';
+export '../Extension/Extension_String.dart';
+
+
+// Firebase
+export '../Firebase/Auth.dart';
+export '../Firebase/Firestore.dart';
+
+
+// Function
+export '../Function/Func_Holidays.dart';
+export '../Function/Func_JumpYAss.dart';
+export '../Function/Func_LoadExcel.dart';
+export '../Function/Func_MinMaxObject.dart';
+
+
+// Model View
+export '../Model/TextFieldCustom.dart';
