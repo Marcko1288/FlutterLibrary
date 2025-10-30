bc377ee0508841b42be3422aaf8591c169da0b8f
Marcko1288 <87115589+Marcko1288@users.noreply.github.com>
2025-10-30 11:02:56 +0100

    Update Firestore.dart

--- lib/Firebase/Firestore.dart
+++ lib/Firebase/Firestore.dart
@@ -10,64 +10,117 @@ import 'Firestore.dart';
 class FireStore {
   final FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
 
-  CollectionReference<Map<String, dynamic>> dirDB(
-      {required String db, required String document, required String value}) {
-    //String db = 'Database';
-    return _firebaseFirestore.collection(db).doc(document).collection(value);
+  CollectionReference<Map<String, dynamic>> dirDB({
+    required String db,
+    required String document,
+    required String value,
+  }) {
+    return FirebaseFirestore.instance
+        .collection(db)
+        .doc(document)
+        .collection(value);
   }
 
-  Future<void> insertFirestore(
-      {required CollectionReference patch,
-      required Map<String, dynamic> map}) async {
+  Future<void> insertFirestore({
+    required CollectionReference patch,
+    required Map<String, dynamic> map,
+    bool test = false,
+  }) async {
     var route = patch.doc(map['uid']);
-    if (!Uri.base.toString().contains('localhost')) {
-       await route.set(map).onError((error, stackTrace) => print('$error'));
+    if (!test) {
+      try {
+        await route.set(map).onError((error, stackTrace) => print('$error'));
+      } on FirebaseException catch (e) {
+        print(
+          '❌ Errore durante il caricamento in Firebase FireStore: ${e.code} - ${e.message}',
+        );
+      } catch (e) {
+        print('❌ Errore generico insertFireStorage: $e');
+      }
     }
   }
 
-  Future<void> cancelFireStore(
-      {required CollectionReference patch,
-      required Map<String, dynamic> map}) async {
+  Future<void> cancelFireStore({
+    required CollectionReference patch,
+    required Map<String, dynamic> map,
+    bool test = false,
+  }) async {
     var route = patch.doc(map['uid']);
-    if (!Uri.base.toString().contains('localhost')) {
-       await route.delete().onError((error, stackTrace) => print('$error'));
+    if (!test) {
+      try {
+        await route.delete().onError((error, stackTrace) => print('$error'));
+      } on FirebaseException catch (e) {
+        print(
+          '❌ Errore durante il caricamento in Firebase FireStore: ${e.code} - ${e.message}',
+        );
+      } catch (e) {
+        print('❌ Errore generico insertFireStorage: $e');
+      }
     }
   }
 
-  Future<void> modifyFireStore(
-      {required CollectionReference patch,
-      required Map<String, dynamic> map}) async {
+  Future<void> modifyFireStore({
+    required CollectionReference patch,
+    required Map<String, dynamic> map,
+    bool test = false,
+  }) async {
     map['data_modify'] = DateTime.now();
     var route = patch.doc(map['uid']);
-    if (!Uri.base.toString().contains('localhost')) {
-       await route.update(map).onError((error, stackTrace) => print('$error'));
+    if (!test) {
+      try {
+        await route.update(map).onError((error, stackTrace) => print('$error'));
+      } on FirebaseException catch (e) {
+        print(
+          '❌ Errore durante il caricamento in Firebase FireStore: ${e.code} - ${e.message}',
+        );
+      } catch (e) {
+        print('❌ Errore generico insertFireStorage: $e');
+      }
     }
   }
 
-  Future<List<Map<String, dynamic>>> queryFireStore(
-      {required CollectionReference patch,
-      TypeQuery type = TypeQuery.NL,
-      String campo = '',
-      String value = ''}) async {
+  Future<List<Map<String, dynamic>>> queryFireStore({
+    required CollectionReference patch,
+    TypeQuery type = TypeQuery.NL,
+    String campo = '',
+    String value = '',
+  }) async {
     List<Map<String, dynamic>> map = [];
-    var route =
-        patchQuery(patch: patch, type: type, campo: campo, value: value);
-    var elementQuery = await route.get().then((value) {
-      for (var element in value.docs) {
-        map.add(element.data() as Map<String, dynamic>);
-      }
-    }, onError: (e) {
-      print("Error completing: $e");
+
+    try {
+      var route = patchQuery(
+        patch: patch,
+        type: type,
+        campo: campo,
+        value: value,
+      );
+      var elementQuery = await route.get().then(
+        (value) {
+          for (var element in value.docs) {
+            map.add(element.data() as Map<String, dynamic>);
+          }
+        },
+        onError: (e) {
+          print("Error completing: $e");
+          return [];
+        },
+      );
+
+      return map;
+    } on FirebaseException catch (e) {
       return [];
-    }); //=> print("Error completing: $e"));
-    return map;
+    } catch (e) {
+      print('❌ Errore generico insertFireStorage: $e');
+      return [];
+    }
   }
 
-  Query patchQuery(
-      {required CollectionReference patch,
-      TypeQuery type = TypeQuery.NL,
-      String campo = '',
-      String value = ''}) {
+  Query patchQuery({
+    required CollectionReference patch,
+    TypeQuery type = TypeQuery.NL,
+    String campo = '',
+    String value = '',
+  }) {
     switch (type) {
       case TypeQuery.EQ:
         return patch.where(campo, isEqualTo: value);
@@ -84,3 +137,82 @@ class FireStore {
     }
   }
 }
+
+
+// class FireStore {
+//   final FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
+
+//   CollectionReference<Map<String, dynamic>> dirDB(
+//       {required String db, required String document, required String value}) {
+//     //String db = 'Database';
+//     return _firebaseFirestore.collection(db).doc(document).collection(value);
+//   }
+
+//   Future<void> insertFirestore(
+//       {required CollectionReference patch,
+//       required Map<String, dynamic> map}) async {
+//     var route = patch.doc(map['uid']);
+//     if (!Uri.base.toString().contains('localhost')) {
+//        await route.set(map).onError((error, stackTrace) => print('$error'));
+//     }
+//   }
+
+//   Future<void> cancelFireStore(
+//       {required CollectionReference patch,
+//       required Map<String, dynamic> map}) async {
+//     var route = patch.doc(map['uid']);
+//     if (!Uri.base.toString().contains('localhost')) {
+//        await route.delete().onError((error, stackTrace) => print('$error'));
+//     }
+//   }
+
+//   Future<void> modifyFireStore(
+//       {required CollectionReference patch,
+//       required Map<String, dynamic> map}) async {
+//     map['data_modify'] = DateTime.now();
+//     var route = patch.doc(map['uid']);
+//     if (!Uri.base.toString().contains('localhost')) {
+//        await route.update(map).onError((error, stackTrace) => print('$error'));
+//     }
+//   }
+
+//   Future<List<Map<String, dynamic>>> queryFireStore(
+//       {required CollectionReference patch,
+//       TypeQuery type = TypeQuery.NL,
+//       String campo = '',
+//       String value = ''}) async {
+//     List<Map<String, dynamic>> map = [];
+//     var route =
+//         patchQuery(patch: patch, type: type, campo: campo, value: value);
+//     var elementQuery = await route.get().then((value) {
+//       for (var element in value.docs) {
+//         map.add(element.data() as Map<String, dynamic>);
+//       }
+//     }, onError: (e) {
+//       print("Error completing: $e");
+//       return [];
+//     }); //=> print("Error completing: $e"));
+//     return map;
+//   }
+
+//   Query patchQuery(
+//       {required CollectionReference patch,
+//       TypeQuery type = TypeQuery.NL,
+//       String campo = '',
+//       String value = ''}) {
+//     switch (type) {
+//       case TypeQuery.EQ:
+//         return patch.where(campo, isEqualTo: value);
+//       case TypeQuery.MI:
+//         return patch.where(campo, isLessThan: value);
+//       case TypeQuery.MIU:
+//         return patch.where(campo, isLessThanOrEqualTo: value);
+//       case TypeQuery.MA:
+//         return patch.where(campo, isGreaterThan: value);
+//       case TypeQuery.MAU:
+//         return patch.where(campo, isGreaterThanOrEqualTo: value);
+//       case TypeQuery.NL:
+//         return patch;
+//     }
+//   }
+// }
